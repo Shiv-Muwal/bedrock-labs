b69e6c04e33ef3616da6ba932c049ed027d0542a
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-24 15:53:39 +0530

    add socialTenor jsx

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Future from "./components/Future"
+import SocialTensor from './components/SocialTensor';
 
 function App() {
   return (
     <div className="App">
       <Future/>
-
+      <SocialTensor />
       <BrowserRouter>
         <Routes>
           {/* <Route exact path="/about" element={<VideoBackground />} />
           <Route exact path="/contact" element={<Contact />} /> */}
+
         </Routes>
       </BrowserRouter>
     </div>
