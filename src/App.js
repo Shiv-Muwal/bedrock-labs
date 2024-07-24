5305fb354032fd63b996b817c17616e6e411108a
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-07-24 15:15:44 +0530

    file add

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutUs from "./components/AboutUs";
-import Contact from "./components/Contact";
+import BedRock from './components/BedRock';
 
 function App() {
   return (
     <div className="App">
+      <BedRock/>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/about" element={<AboutUs />} />
-          <Route exact path="/contact" element={<Contact />} />
+          {/* <Route exact path="/about" element={<AboutUs />} />
+          <Route exact path="/contact" element={<Contact />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/BedRock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BedRock = () => {
+  return (
+  <div className=" min-vh-100 bg-black">
+
+
+  </div>
+  );
+};
+
+export default BedRock;
