d0302f5d36534706fc8cf08a382a635bb7f90738
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-25 11:00:30 +0530

    remove list style class

--- src/components/BedRockIcons.jsx
+++ src/components/BedRockIcons.jsx
@@ -14,10 +14,10 @@ const BedRockIcons = () => {
         </a>
         <div className="flex-grow-1 w-100 d-flex align-items-center justify-content-center">
         <ul className='ps-0 mb-0 d-flex gap_40 gap_32'>
-                    <li className='list-unstyled'><a href="https://telegram.org/" target='_blank'><img className='social_links' src={telegram} alt="telegram" /></a></li>
-                    <li className='list-unstyled'><a href="https://discord.com/channels/984345847628181504/999268522427437086" target='_blank'><img className='social_links' src={discord} alt="discord" /></a></li>
-                    <li className='list-unstyled'><a href="https://x.com/?lang=en" target='_blank'><img className='social_links' src={twitter} alt="twitter" /></a></li>
-                    <li className='list-unstyled'><a href="https://medium.com/" target='_blank'><img className='social_links' src={exclude} alt="exclude" /></a></li>
+                    <li><a href="https://telegram.org/" target='_blank'><img className='social_links' src={telegram} alt="telegram" /></a></li>
+                    <li><a href="https://discord.com/channels/984345847628181504/999268522427437086" target='_blank'><img className='social_links' src={discord} alt="discord" /></a></li>
+                    <li><a href="https://x.com/?lang=en" target='_blank'><img className='social_links' src={twitter} alt="twitter" /></a></li>
+                    <li><a href="https://medium.com/" target='_blank'><img className='social_links' src={exclude} alt="exclude" /></a></li>
                 </ul>
         </div>
       </Container>
