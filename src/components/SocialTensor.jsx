05cb3cf5bf1915f99f7ff6093b8436e36fc97918
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-26 09:36:42 +0530

    add css

--- src/components/SocialTensor.jsx
+++ src/components/SocialTensor.jsx
@@ -12,14 +12,14 @@ const SocialTensor = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseOver = (e) => {
-    const centerSlide = e.target.closest('.slick-center');
+    const centerSlide = e.target.closest('.slick-center p');
     if (centerSlide) {
       setIsHovered(true);
     }
   };
 
   const handleMouseOut = (e) => {
-    const centerSlide = e.target.closest('.slick-center');
+    const centerSlide = e.target.closest('.slick-center p');
     if (centerSlide) {
       setIsHovered(false);
     }
@@ -44,33 +44,33 @@ const SocialTensor = () => {
     <div className='bg-black overflow-hidden position-relative'>
       <Container className='d-flex flex-column min-vh-100 px_30'>
         <div className="pt_40">
-          <a href='#'>
+          <a href='#' className="position-absolute z-3">
             <img src={logo_nav} alt="logo_nav" />
           </a>
         </div>
         <div className="d-flex overflow-hidden flex-column flex-grow-1 justify-content-center align-items-center">
           <Slider {...settings} className="vertical-slider overflow-hidden">
-            {['Aperture', 'Aperture', 'NicheTensor', 'THELASTONE', 'INFINI', 'INFINI', 'INFINI', 'INFINI', 'INFINI', 'INFINI'].map((item, index) => (
+            {['SocialTensor', 'Aperture','Aperture', 'NicheTensor', 'THELASTONE', 'INFINI', 'TourGame', 'NicheTensor', 'INFINI', 'CyberConnect', 'INFINI','THELASTONE','THELASTONE',].map((item, index) => (
               <div
                 key={index}
-                className="d-flex align-items-center justify-content-center"
+                className="d-flex align-items-center justify-content-center d-inline-block"
                 onMouseOver={handleMouseOver}
                 onMouseOut={handleMouseOut}
               >
-                <p className="mb-0 show_box text_42 lh_52 slider_text text-center d-inline-block font_Gilroy_SemiBoldItalic fst-italic">
+                <p className="mb-0 text_80 lh_52 slider_text text-center position-relative d-inline-block font_Gilroy_SemiBoldItalic fst-italic">
                   {item}
                 </p>
               </div>
             ))}
           </Slider>
-          <div className="d-flex align-items-center gap_20 w-100 justify-content-end position-absolute slider-box-common">
-            <div className={`slider-box sliderbox1 d-flex align-items-end justify-content-between w-100 ${isHovered ? 'hovered' : ''}`}>
-              <p className="text-white fst-italic text_12 slider-box-text d-inline-block">UI UX</p>
+          <div className="d-flex align-items-center gap_20 w-100 justify-content-end position-absolute slider-box-common z-0">
+            <div className={`slider-box sliderbox1 d-flex align-items-end justify-content-xl-between w-100 ${isHovered ? 'hovered' : ''}`}>
+              <p className="text-white fst-italic text_12 slider-box-text d-inline-block text-nowrap">UI UX</p>
               <img src={social_sensor} alt="" className="slider-img" />
             </div>
             <div className={`slider-box sliderbox2 d-flex align-items-end justify-content-between w-100 ${isHovered ? 'hovered' : ''}`}>
-              <p className="text-white fst-italic text_12 slider-box-text d-inline-block">UI UX</p>
-              <img src={social_sensor} alt="" className="slider-img" />
+              <p className="text-white fst-italic text_12 slider-box-text d-inline-block text-uppercase">Branding</p>
+              <img src={social_tensor} alt="" className="slider-img" />
             </div>
           </div>
         </div>
